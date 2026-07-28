5077e06e0e0f38194389e1f6ac249c925a1684df
Lordlance-lanre <96908789+Lordlance-lanre@users.noreply.github.com>
2026-07-28 13:14:16 +0100

    Update main.go

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	// Enable CORS middleware before routes setup
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://blog-post-app-kohl.vercel.app/"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://blog-post-qdvthl7qd-lordlancelanres-projects.vercel.app/"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowCredentials: true,
