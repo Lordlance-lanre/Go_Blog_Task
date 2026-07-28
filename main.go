ecb68aa673627d8eb2162798450fbdd338a5b774
Lordlance-lanre <96908789+Lordlance-lanre@users.noreply.github.com>
2026-07-28 13:11:07 +0100

    push cors error fix

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 
 	// Enable CORS middleware before routes setup
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173"},
+		AllowOrigins:     []string{"http://localhost:3000", "http://localhost:5173", "https://blog-post-app-kohl.vercel.app/"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowCredentials: true,
